3ab7785ac9fffd2a3c712e7a91af6c274dde9354
Simran Rawat <rawatsimran2007@gmail.com>
2026-02-22 15:39:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
+
+export default App;
